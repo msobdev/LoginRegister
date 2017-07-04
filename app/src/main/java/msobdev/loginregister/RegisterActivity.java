b55ac502a94e1e1357386b34e572298600f366b9
msobdev <msobdev@gmail.com>
2017-07-04 19:50:38 +0200

    Registering user and sending data to 000webhosting database using php script

--- app/src/main/java/msobdev/loginregister/RegisterActivity.java
+++ app/src/main/java/msobdev/loginregister/RegisterActivity.java
@@ -1,9 +1,19 @@
 package msobdev.loginregister;
 
+import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -20,6 +30,45 @@ public class RegisterActivity extends AppCompatActivity {
 
         final Button regButton = (Button) findViewById(R.id.regButton);
 
+        regButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                final String name = etName.getText().toString();
+                final String username = etUsername.getText().toString();
+                final String password = etPassword.getText().toString();
+                final int age = Integer.parseInt(etAge.getText().toString());
 
+                Response.Listener<String> responseListener = new Response.Listener<String>(){
+
+                    @Override
+                    public void onResponse(String response) {
+
+                        try {
+                            JSONObject jsonObject = new JSONObject(response);
+                            boolean success = jsonObject.getBoolean("success");
+
+                            if(success){
+                                Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+                                RegisterActivity.this.startActivity(intent);
+                            }else{
+                                AlertDialog.Builder builder = new AlertDialog.Builder(RegisterActivity.this);
+                                builder.setMessage("Register Failed")
+                                        .setNegativeButton("Retry", null)
+                                        .create()
+                                        .show();
+                            }
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+                    }
+                };
+
+                RegisterRequest registerRequest = new RegisterRequest(name, username, age, password, responseListener);
+                RequestQueue queue = Volley.newRequestQueue(RegisterActivity.this);
+                queue.add(registerRequest);
+            }
+        });
     }
 }
